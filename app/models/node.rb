8ed1ed1e96a8b7e1d09fd181dca3e8cefc6dc08d
Geoff Massanek <gmassanek@gmail.com>
2011-06-16 21:27:43 -0400

    node.relationships and node.related_nodes

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,9 +1,23 @@
 class Node < ActiveRecord::Base
-  #attr_accessor :title, :description
+  has_many :relationships1, :class_name => 'Relationship', :foreign_key => 'node1_id'
+  has_many :relationships2, :class_name => 'Relationship', :foreign_key => 'node2_id'
   validates :title, :presence => true
   validates :description, :presence => true
   def to_s
-    self.title
+    title
+  end
+  def relationships
+    relationships1<<relationships2
+  end
+  def related_nodes
+    nodes = []
+    relationships1.each do |r|
+      nodes<<r.node2
+    end
+    relationships2.each do |r|
+      nodes<<r.node1
+    end
+    return nodes
   end
 end
 
--- spec/models/nodes_spec.rb
+++ spec/models/nodes_spec.rb
@@ -11,5 +11,31 @@ describe Node do
     @node.description = nil
     @node.should_not be_valid
   end
+  it "responds to relationships" do
+    @node = Fabricate(:node)
+    @node.should respond_to(:relationships)
+  end
+  it ".relationships gives you all relationships that include it" do
+    @node = Fabricate(:node)
+    @node2 = Fabricate(:node)
+    @node3 = Fabricate(:node)
+    @rel = Relationship.create(:node1 => @node, :node2 => @node2, :sentence1 => "%1 %2", :sentence2 => "%2 %1")
+    @rel2 = Relationship.create(:node1 => @node3, :node2 => @node, :sentence1 => "%1 %2", :sentence2 => "%2 %1")
+    @node.relationships.should include(@rel)
+    @node.relationships.should include(@rel2)
+  end
+  it "responds to related_nodes" do
+    @node = Fabricate(:node)
+    @node.should respond_to(:related_nodes)
+  end
+  it ".related_nodes gives you all related nodes" do
+    @node = Fabricate(:node)
+    @node2 = Fabricate(:node)
+    @node3 = Fabricate(:node)
+    @rel = Relationship.create(:node1 => @node, :node2 => @node2, :sentence1 => "%1 %2", :sentence2 => "%2 %1")
+    @rel2 = Relationship.create(:node1 => @node3, :node2 => @node, :sentence1 => "%1 %2", :sentence2 => "%2 %1")
+    @node.related_nodes.should include(@node2)
+    @node.related_nodes.should include(@node3)
+  end
 end
 
