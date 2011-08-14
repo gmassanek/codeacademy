ee622994c47be985b1f575f751531acf414e22e7
Geoff Massanek <gmassanek@gmail.com>
2011-08-13 20:27:12 -0400

    adding db:seed

--- Rakefile
+++ Rakefile
@@ -3,5 +3,6 @@
 
 require File.expand_path('../config/application', __FILE__)
 require 'rake'
+include Rake::DSL
 
 Ecosystem::Application.load_tasks
---
+++ db/seeds/development.rb
@@ -0,0 +1,4 @@
+#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
+#   Mayor.create(:name => 'Daley', :city => cities.first)
+github = Fabricate(:node, :title => "Rspec")
+github = Fabricate(:node, :title => "Twitter")
