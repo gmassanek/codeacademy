081d7953faa982129e076c589cb4af29839a53f3
Geoff Massanek <gmassanek@gmail.com>
2011-08-27 11:52:40 -0400

    strangely broken tests, but base sign up functionality working

--- spec/integration/authentication_spec.rb
+++ spec/integration/authentication_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
-describe "Site Authentication" do
+describe "Site Authentication", :js => true do
+  let (:user) {Fabricate(:user)}
   before do
-    visit destroy_user_session_path
+    #visit destroy_user_session_path
   end
-  it "has a sign up link on the home page when you are not signed in", :js => true do
+  it "has a sign up link on the home page when you are not signed in" do
     visit root_path
     page.should have_link('Sign up')
   end
-  it "registers you when you fill in username, email, password", :js => true, :broken => true do
+  it "registers you when you fill in username, email, password", :broken => true do
     visit new_user_registration_path
     page.fill_in 'Email', :with => 'foo@example.com'
     page.fill_in 'Password', :with => 'password'
@@ -15,10 +16,11 @@ describe "Site Authentication" do
     page.click_link_or_button 'Sign up'
     save_and_open_page
     page.should have_content("Welcome")
-    visit destroy_user_session_path
+  end
+  it "lets a user log in", :broken => true do
     visit new_user_session_path
-    page.fill_in 'Email', :with => 'foo@example.com'
-    page.fill_in 'Password', :with => 'password'
+    page.fill_in 'Email', :with => user.email
+    page.fill_in 'Password', :with => user.password
     page.click_link_or_button 'Sign in'
     page.should have_content("Signed in successfully")
   end
