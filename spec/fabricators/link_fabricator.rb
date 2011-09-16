bde0a4ca131008606bc75c90c957f296408cef7f
Geoff Massanek <gmassanek@gmail.com>
2011-09-15 22:13:52 -0400

    fixing master to not have an issue with link fabricator

--- spec/fabricators/link_fabricator.rb
+++ spec/fabricators/link_fabricator.rb
@@ -1,7 +1,3 @@
-Fabricator(:user_helpfulness_rating) do
-  user! {Fabractate(:user)}
-  url { |link| "http://www.#{link.linkable.to_s.gsub(" ", "")}.com"}
-end
 Fabricator(:node_link, :from => :link) do
   linkable! {Fabricate(:node)}
   url { |link| "http://www.#{link.linkable.to_s.gsub(" ", "")}.com"}
