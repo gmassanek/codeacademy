64f58246775ba103bcf7c4b0e3afa512c6564d57
Geoff Massanek <gmassanek@gmail.com>
2011-09-19 23:03:22 -0400

    accidently overwrote site handle spec file, copied it back in from github

--- spec/models/site_handle_spec.rb
+++ spec/models/site_handle_spec.rb
@@ -1,5 +1,45 @@
 require 'spec_helper'
 
-describe SiteHandle do
+describe "SiteHandle" do
+  describe "new siteHandle" do
+    before do
+      @node = Fabricate(:node)
+      visit new_node_site_handle_path(@node)
+    end
+    it "saves new siteHandles" do
+      page.fill_in 'Github', :with => "gmassanek"
+      page.click_button 'Create Site handle'
+      page.should have_content("Site Handle created")
+    end
+    it "shows errors" do
+    end
+  end
+  describe "edit siteHandle" do
+    before do
+      @siteHandle = Fabricate(:site_handle)
+      visit edit_polymorphic_path([@siteHandle.item, @siteHandle])
+    end
+    it "can edit siteHandles" do
+      page.fill_in 'Github', :with => "gmassanek"
+      page.click_button 'Update Site handle'
+      page.should have_content("Site Handle updated")
+    end
+    it "shows errors" do
+    end
+  end
+  describe "show siteHandle" do
+    before do
+      @siteHandle = Fabricate(:site_handle)
+      @node = @siteHandle.item
+    end
+    it "shows the siteHandle title" do
+      visit polymorphic_path([@node, @siteHandle])
+      page.should have_css('h2', :text => @siteHandle.to_s)
+    end
+    it "shows the siteHandle Url" do
+      visit polymorphic_path([@node, @siteHandle])
+      page.should have_content("#{@siteHandle.github}")
+    end
+  end
 end
 
