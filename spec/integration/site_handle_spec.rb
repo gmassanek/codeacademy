cc4103f53c37bdf2ddc08f5436fec0b115cbe539
Geoff Massanek <gmassanek@gmail.com>
2011-09-19 22:45:59 -0400

    Added test for home page, added relationships as a sublist on home page

--- spec/integration/site_handle_spec.rb
+++ spec/integration/site_handle_spec.rb
@@ -1,44 +1,20 @@
 require 'spec_helper'
 
-describe "SiteHandle" do
-  describe "new siteHandle" do
-    before do
-      @node = Fabricate(:node)
-      visit new_node_site_handle_path(@node)
+describe "Pages" do
+  describe "home page" do
+    it "has all the Nodes" do
+      3.times do Fabricate(:node) end
+      visit home_path
+      Node.all.each do |n|
+        page.should have_content n.to_s
+      end
     end
-    it "saves new siteHandles" do
-      page.fill_in 'Github', :with => "gmassanek"
-      page.click_button 'Create Site handle'
-      page.should have_content("Site Handle created")
-    end
-    it "shows errors" do
-    end
-  end
-  describe "edit siteHandle" do
-    before do
-      @siteHandle = Fabricate(:site_handle)
-      visit edit_polymorphic_path([@siteHandle.item, @siteHandle])
-    end
-    it "can edit siteHandles" do
-      page.fill_in 'Github', :with => "gmassanek"
-      page.click_button 'Update Site handle'
-      page.should have_content("Site Handle updated")
-    end
-    it "shows errors" do
-    end
-  end
-  describe "show siteHandle" do
-    before do
-      @siteHandle = Fabricate(:site_handle)
-      @node = @siteHandle.item
-    end
-    it "shows the siteHandle title" do
-      visit polymorphic_path([@node, @siteHandle])
-      page.should have_css('h2', :text => @siteHandle.to_s)
-    end
-    it "shows the siteHandle Url" do
-      visit polymorphic_path([@node, @siteHandle])
-      page.should have_content("#{@siteHandle.github}")
+    it "has all the relationships" do
+      3.times do Fabricate(:relationship) end
+      visit home_path
+      Relationship.all.each do |n|
+        page.should have_content n.to_s
+      end
     end
   end
 end
