5a7cc2502cd9f6f00b163f52b74a761b2a71ce69
Geoff Massanek <gmassanek@gmail.com>
2011-10-07 19:08:50 -0500

    change homepage to nodes index

--- config/routes.rb
+++ config/routes.rb
@@ -61,7 +61,7 @@ Ecosystem::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   #@firstNode = Node.first
-  root :to => "pages#home"
+  root :to => "nodes#index"
   #map.root :controller => 'nodes', :action => 'show', :node => @firstNode
 
 
